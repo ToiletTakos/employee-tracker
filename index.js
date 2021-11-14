a0298d4acb2ba40405cd1f2b967d11e936438297
Dan <dan.pandolfo@gmail.com>
2021-11-13 22:17:59 -0500

    created the last three things. Adding a role, adding an employee and updating an employee role

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const promptBusiness = () => {
             ]
         }
     ])
-    .then(function(choice){
+    .then((choice) => {
         switch(choice.selectionMenu){
             case "View all departments":
                 promptDepartments();
@@ -47,7 +47,7 @@ const promptBusiness = () => {
                 promptUpdateRole();
                 break;
             default:
-                sayBye();
+                console.log("Thanks for using the Database! Have a nice day!");
                 break;
         }
     })
@@ -101,28 +101,204 @@ const promptEmployees = () => {
 }
 
 const promptAddDepartment = () => {
-    console.log("Add departments");
-    promptBusiness();
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: "What is the new Department's name?",
+            validate: department => {
+                if(department) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter a new Department's name!");
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(body => {
+        const sql = `INSERT INTO department (department)
+                    values (?)`;
+
+        const params = [body.department];
+
+        db.query(sql, params, (err, result) => {
+            if(err) {
+                console.log(err);
+                promptAddDepartment();
+            };
+            console.log('Department added!')
+            promptBusiness();
+        });
+
+    })
 }
 
 const promptAddRole = () => {
-    console.log("Add role");
-    promptBusiness();
+    const Sql = `SELECT id AS value, department FROM department`;
+        db.query(Sql, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "roleTitle",
+                message: "What's the title for the role you're adding?",
+                validate: roleTitle => {
+                    if(roleTitle) {
+                        return true;
+                    }
+                    else {
+                        console.log("Please enter a role title!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "roleSalary",
+                message: "What's the salary for the role?",
+                validate: roleSalary => {
+                    if(roleSalary) {
+                        return true;
+                    }
+                    else {
+                        console.log("Please enter a salary!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "list",
+                name: "roleDept",
+                message: "Which department does the role belong to?",
+                choices: results,
+            },
+            ])
+            .then((answers) => {
+                const roleSql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+                const params = [answers.roleTitle, answers.roleSalary, answers.roleDept];
+                db.query(roleSql, params, function (err, results) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log("Role added!");
+                    promptBusiness();
+                });
+            });
+        });
 }
 
 const promptAddEmployee = () => {
-    console.log("Add employee");
-    promptBusiness();
+    const sql = `SELECT id AS value, title AS name FROM role`;
+          db.query(sql, (err, roles) => {
+            if (err) {
+              console.log(err);
+            }
+            const nameSql = `SELECT id AS value, CONCAT(first_name, " ", last_name) AS name FROM employee`;
+            db.query(nameSql, (err, managers) => {
+              if (err) {
+                console.log(err);
+              }
+              inquirer.prompt([
+                  {
+                    type: "input",
+                    name: "firstName",
+                    message: "What's' first name of the employee you want to add?",
+                    validate: firstName => {
+                        if(firstName) {
+                            return true;
+                        }
+                        else {
+                            console.log("Please enter a name!");
+                            return false;
+                        }
+                    }
+                  },
+                  {
+                    type: "input",
+                    name: "lastName",
+                    message: "What's the last name of the employee you want to add?",
+                    validate: lastName => {
+                        if(lastName) {
+                            return true;
+                        }
+                        else {
+                            console.log("Please enter a last name!");
+                            return false;
+                        }
+                    }
+                  },
+                  {
+                    type: "list",
+                    name: "role",
+                    message: "What's the role of this employee?",
+                    choices: roles,
+                  },
+                  {
+                    type: "list",
+                    name: "manager",
+                    message: "Who is the employee's manager?",
+                    choices: managers,
+                  },
+                ])
+                .then((answers) => {
+                    const employeeSql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+                    const params = [answers.firstName, answers.lastName, answers.role, answers.manager];
+                    db.query(employeeSql, params, function (err, results) {
+                      if (err) {
+                        console.log(err);
+                      }
+                      console.log("Employee added!");
+                      promptBusiness();
+                    });
+                });
+            });
+        });
+    
 }
 
 const promptUpdateRole = () => {
-    console.log("Update employees role");
-    promptBusiness();
-}
-
-const sayBye = () => {
-    console.log("Thanks for using the Database! Have a nice day!");
-}
+    const sql = `SELECT id AS value, title AS name FROM role`;
+        db.query(sql, (err, roles) => {
+            if (err) {
+                console.log(err);
+            }
+            const updateRoleSql = `SELECT id AS value, CONCAT(first_name, " ", last_name) AS name FROM employee`;
+            db.query(updateRoleSql, (err, employees) => {
+                if (err) {
+                console.log(err);
+            }
+            inquirer.prompt([
+                {
+                    type: "list",
+                    name: "employee",
+                    message: "Who is the employee that needs updating?",
+                    choices: employees,
+                },
+                {
+                    type: "list",
+                    name: "updatedRole",
+                    message:'What is the new role?',
+                    choices: roles,
+                },
+                ])
+                .then((answers) => {
+                    const updatedRolesql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+                    const params = [answers.updatedRole, answers.employee];
+                    db.query(updatedRolesql, params, (err, results) => {
+                      if (err) {
+                        console.log(err);
+                      }
+                      console.log("Employee role Updated!");
+                      promptBusiness();
+                    });
+                });
+            });
+        });
+};
 
 
 // Connect to database
@@ -138,4 +314,13 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
+function welcomeMessage() {
+    console.log('-------------------')
+    console.log('|   Welcome to    |')
+    console.log('|  Your Employee  |')
+    console.log('|     Manager     |')
+    console.log('-------------------')
+};
+
+welcomeMessage();
 promptBusiness();
